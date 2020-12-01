483683d3dcbf7fb3e8b2a53daec8f20b104f5fcf
ezist <maxwell1998r@gmail.com>
2020-12-01 15:14:18 -0600

    Initial commit
    Signed-off-by: ezist <maxwell1998r@gmail.com>

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Plugin } = require('powercord/entities')
+
+module.exports = class QuickToggler extends Plugin {
+  async startPlugin () {
+    this._themesEnabled = true
+    this._pluginsEnabled = true
+    document.addEventListener('keydown', this.onKeyDownWrapper.bind(this))
+  }
+
+  pluginWillUnload () { document.removeEventListener(this.onKeyDownWrapper.bind(this)) }
+
+  onKeyDown (e) {
+    switch (e.code) {
+      case 'F6':
+        powercord.pluginManager.plugins.forEach(v => {
+          if (v.entityID.startsWith('pc-')) return
+          powercord.pluginManager[this._pluginsEnabled ? 'disable' : 'enable'](v.entityID)
+        })
+        this._pluginsEnabled = !this._pluginsEnabled
+        break
+      case 'F7': {
+        powercord.styleManager.themes.forEach(v => v.entityID && powercord.styleManager[this._themesEnabled ? 'disable' : 'enable'](v.entityID))
+
+        let moduleManager = powercord.pluginManager.get('pc-moduleManager')
+        if (this._themesEnabled)
+          document.querySelector('#powercord-quickcss')?.remove()
+        else moduleManager._loadQuickCSS.bind(moduleManager)()
+
+        this._themesEnabled = !this._themesEnabled
+        break
+      }
+    }
+  }
+
+  onKeyDownWrapper (e) { this.onKeyDown(e) }
+}
