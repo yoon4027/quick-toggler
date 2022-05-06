a78dee07fe5dc56f43c2dd3947469ad751341b23
yoon4027 <yooooonus@gmail.com>
2022-05-06 18:02:24 +0500

    [feat] Added toasts

--- index.js
+++ index.js
@@ -1,42 +1,93 @@
-const { Plugin } = require('powercord/entities')
+/* eslint-disable  */
+const { Plugin } = require("powercord/entities");
 
 module.exports = class QuickToggler extends Plugin {
-  async startPlugin () {
-    this._themesEnabled = true
-    this._pluginsEnabled = true
-    this._enabledPlugins = []
-    document.addEventListener('keydown', this.onKeyDownWrapper.bind(this))
+  async startPlugin() {
+    this._themesEnabled = true;
+    this._pluginsEnabled = true;
+    this._enabledPlugins = [];
+    document.addEventListener("keydown", this.onKeyDownWrapper.bind(this));
   }
 
-  pluginWillUnload () { document.removeEventListener(this.onKeyDownWrapper.bind(this)) }
+  pluginWillUnload() {
+    document.removeEventListener(this.onKeyDownWrapper.bind(this));
+  }
 
-  onKeyDown (e) {
+  onKeyDown(e) {
     switch (e.code) {
-      case 'F6':
+      case "F6":
         if (this._pluginsEnabled) {
-          this._enabledPlugins = Array.from(powercord.pluginManager.plugins.values())
-            .filter(p => p.ready)
-            .map(p => p.entityID)
+          this._enabledPlugins = Array.from(
+            powercord.pluginManager.plugins.values()
+          )
+            .filter((p) => p.ready)
+            .map((p) => p.entityID);
         }
-        this._enabledPlugins.forEach(v => {
-          if (v.startsWith('pc-') || v === 'quick-toggler') return
-          powercord.pluginManager[this._pluginsEnabled ? 'disable' : 'enable'](v)
-        })
-        this._pluginsEnabled = !this._pluginsEnabled
-        break
-      case 'F7': {
-        powercord.styleManager[this._themesEnabled ? 'unloadThemes' : 'loadThemes']()
+        this._enabledPlugins.forEach((v) => {
+          if (v.startsWith("pc-") || v === "quick-toggler") {
+            return;
+          }
+          powercord.pluginManager[this._pluginsEnabled ? "disable" : "enable"](
+            v
+          );
+        });
+        this._pluginsEnabled = !this._pluginsEnabled;
 
-        let moduleManager = powercord.pluginManager.get('pc-moduleManager')
-        if (this._themesEnabled)
-          document.querySelector('#powercord-quickcss')?.remove()
-        else moduleManager._loadQuickCSS.bind(moduleManager)()
+        const pluginToast = `toggler-toast-plugin-${Date.now()}`;
 
-        this._themesEnabled = !this._themesEnabled
-        break
+        powercord.api.notices.sendToast(pluginToast, {
+          header: this._pluginsEnabled
+            ? "Plugins have been enabled"
+            : "Plugins now have been disabled",
+          buttons: [
+            {
+              text: "Dismiss",
+              look: "ghost",
+              size: "small",
+              onClick: () => powercord.api.notices.closeToast(pluginToast),
+            },
+          ],
+          timeout: 2000,
+        });
+
+        break;
+      case "F7": {
+        powercord.styleManager[
+          this._themesEnabled ? "unloadThemes" : "loadThemes"
+        ]();
+
+        const moduleManager = powercord.pluginManager.get("pc-moduleManager");
+        if (this._themesEnabled) {
+          document.querySelector("#powercord-quickcss")?.remove();
+        } else {
+          moduleManager._loadQuickCSS.bind(moduleManager)();
+        }
+
+        this._themesEnabled = !this._themesEnabled;
+
+        const themeToast = `toggler-toast-theme-${Date.now()}`;
+
+        powercord.api.notices.sendToast(plugthemeToastinToast, {
+          header: this._pluginsEnabled
+            ? "Themes have been enabled"
+            : "Themes now have been disabled",
+          buttons: [
+            {
+              text: "Dismiss",
+              look: "ghost",
+              size: "small",
+              onClick: () => powercord.api.notices.closeToast(themeToast),
+            },
+          ],
+          timeout: 2000,
+        });
+
+        break;
       }
     }
   }
 
-  onKeyDownWrapper (e) { this.onKeyDown(e) }
-}
+  onKeyDownWrapper(e) {
+    this.onKeyDown(e);
+  }
+};
